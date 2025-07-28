d5fdf5c6f6f4bfd0c4cb12745d290a1c6a63ba44
Rafael Miguel <rafaelmiguel0202@gmail.com>
2025-07-28 18:21:05 -0300

    adding mass property to fluids

--- include/ParticleSystem.h
+++ include/ParticleSystem.h
@@ -9,8 +9,9 @@ struct Particle {
     glm::vec3 Position, Velocity;
     glm::vec4 Color;
     float     Life;
+    float     Mass;
 
-    Particle() : Position(0.0f), Velocity(0.0f), Color(1.0f), Life(0.0f) { }
+    Particle() : Position(0.0f), Velocity(0.0f), Color(1.0f), Life(0.0f), Mass(0.0f) { }
 };
 
 class ParticleSystem
@@ -20,8 +21,10 @@ public:
     ~ParticleSystem();
 
     void Update(float dt, const glm::vec3& gravityObjectPos, unsigned int newParticles, glm::vec3 spawnOffset = glm::vec3(0.0f));
-    
     void Render(const glm::mat4& view, const glm::mat4& projection);
+
+    glm::vec3 CenterOfMass;
+    float     TotalMass;
 
 private:
     std::vector<Particle> particles;
